f61dee29b7d84a5c9b3dc0175e0ff6e6713bae00
Srirangan <srirangan@gmail.com>
2011-11-04 07:30:47 +0530

    Fix for Issue #1 - https://github.com/Srirangan/notifer.js/issues/1

--- notifier.js
+++ notifier.js
@@ -2,15 +2,17 @@ $(document).ready(function() {
   $("body").append("<div id='notificationsContainer'></div>");
 });
 
+function escapeHtml(string) { return string.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;"); }
+
 var Notifier = {};
 
 Notifier.notify = function(message, title, iconUrl, timeOut) {
   var id = "notif-" + new Date().getTime(), notificationHtml = "";
   notificationHtml += "<div id='" + id + "' class='notification' onclick='$(this).fadeOut()'>";
-  if (iconUrl) notificationHtml += "<img class='icon' src='" + iconUrl + "'/>";
+  if (iconUrl) notificationHtml += "<img class='icon' src='" + escapeHtml(iconUrl) + "'/>";
   notificationHtml += "<div class='text'>";
-  if (title) notificationHtml += "<div class='title'>" + title + "</div>";
-  if (message) notificationHtml += "<div class='message'>" + message + "</div>";
+  if (title) notificationHtml += "<div class='title'>" + escapeHtml(title) + "</div>";
+  if (message) notificationHtml += "<div class='message'>" + escapeHtml(message) + "</div>";
   notificationHtml += "</div>";
   notificationHtml += "</div>";
   $("#notificationsContainer").prepend(notificationHtml);
